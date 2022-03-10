b08cec9b5512a46422bc74a4cd1f99c842a33519
abhishek <harikarthik191@gmail.com>
2022-03-10 11:45:19 +0530

    phone commit

---
+++ src/Header.js
@@ -0,0 +1,21 @@
+import React,{Component} from 'react';
+//const Header=function(props){
+  //  return(
+    //<div style={{textAlign:'center',textTransform:'uppercase',backgroundColor:'black',color:'white',padding:20}}>
+     //{props.heading}
+       // </div>
+    //)
+//}
+
+
+class Header extends Component{
+
+    render(){
+        return(
+            <div style={{textAlign:'center',textTransform:'uppercase',backgroundColor:'black',color:'white',padding:20}}>
+     {this.props.instructor}
+     </div>
+        )
+    }
+}
+export default Header;
---
+++ src/PhoneDirectory.js
@@ -0,0 +1,68 @@
+import React ,{Component} from 'react';
+import AddSubscriber from './AddSubscriber';
+import ShowSubscriber from './ShowSubscriber';
+import {BrowserRouter , Routes, Route } from 'react-router-dom';
+
+class PhoneDirectory extends Component {
+    constructor(){
+        super();
+        this.state={
+            subscriberList:[
+                {
+                    id:1,
+                    name:'Phani',
+                    phone:'9988776655'
+                },
+                {
+                    id:2,
+                    name:'Mohan',
+                    phone:'9977668822'
+                }
+            ]
+        }
+    }
+
+    deleteSubscriberHandler =(subscriberId) =>{
+        let subscriberList=this.state.subscriberList;
+        let subscriberIndex=0;
+        subscriberList.forEach(function(subscriber,index){
+            if(subscriber.id === subscriberId){
+                subscriberIndex=index;
+            }
+        },this)
+        let newSubscribers=subscriberList;
+        subscriberList.splice(subscriberIndex,1);
+        this.setState({
+            subscribers:newSubscribers
+        });
+
+    }
+
+    addSubscriberHandler =(newSubscriber) =>{
+        let subscriberList=this.state.subscriberList;
+        console.log(this.state.subscriberList)
+        if(subscriberList.length>0){
+            newSubscriber.id= subscriberList[subscriberList.length-1].id +1;
+        }else{
+            newSubscriber.id=1;
+        }
+        subscriberList.push(newSubscriber);
+        this.setState({subscriberList:subscriberList});
+        console.log(this.state.subscriberList);
+    }
+    render(){
+        return(
+            // <AddSubscriber addSubscriberHandler={this.addSubscriberHandler}/>
+            // <ShowSubscriber subscriberList={this.state.subscriberList} />
+            <BrowserRouter>
+            <Routes>
+                <Route exact path="/" element={<ShowSubscriber subscriberList={this.state.subscriberList} deleteSubscriberHandler={this.deleteSubscriberHandler} />}/>
+                <Route exact path="/add" element={<AddSubscriber addSubscriberHandler={this.addSubscriberHandler}/>}/>
+            </Routes>
+            </BrowserRouter>
+        )
+    }
+}
+
+export default PhoneDirectory;
+
